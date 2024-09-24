a677df6a2e1a35994f2f93c0c608d295ae7377e4
IsmaelCesar <leamscesar@gmail.com>
2024-09-24 11:40:40 -0300

    ano nascimento

--- unidade_2/arquivos/criacao_de_arquivos.c
+++ unidade_2/arquivos/criacao_de_arquivos.c
@@ -5,8 +5,8 @@ int main(){
     FILE *f = fopen("meu_arquivo.txt", "w");
     
     char nome[] = "Dino da Silva Sauro";
-    int tempo_nascimento = 65000890;
-    fprintf(f, "Nome: %s, nascido em: %d A.C.", nome, tempo_nascimento);
+    int ano_nascimento = 65000890;
+    fprintf(f, "Nome: %s, nascido em: %d A.C.", nome, ano_nascimento);
 
     fclose(f);
 
