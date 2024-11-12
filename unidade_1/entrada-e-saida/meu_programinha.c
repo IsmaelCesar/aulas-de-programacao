f0ef2494f6b93a5ceba748233a1c9836a808ee33
IsmaelCesar <leamscesar@gmail.com>
2024-11-12 16:25:36 -0300

    12/11/2024

---
+++ unidade_1/entrada-e-saida/meu_programinha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("\v \t Voce \n pegou \n em \n \t bomba!");
+
+    int var1 = 10, var2 = 40, var3 = 10;
+
+    printf("\nVar1: %d , Var2: %d", var1, var2);
+
+    printf("\n%d %d",var3, var2);
+    
+    return 0;
+}
