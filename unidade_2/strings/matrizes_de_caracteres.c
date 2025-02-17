5736f9efd1508f507bd4cf1195d14435efa2f76f
IsmaelCesar <leamscesar@gmail.com>
2025-02-17 16:40:17 -0300

    matrizes de caracteres

---
+++ unidade_2/strings/matrizes_de_caracteres.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    float nome_mat[3][3];
+
+    char nome_pessoas[3][100] = { 
+        "Pessoa 1", 
+        "Pessoa 2",
+        "Pessoa 3"
+    };
+
+    return 0;
+}
