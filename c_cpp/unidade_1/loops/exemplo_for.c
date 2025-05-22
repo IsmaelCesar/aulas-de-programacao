56ef0124fb54be143d2d0bac6abf96d54cd4f627
IsmaelCesar <leamscesar@gmail.com>
2025-05-22 16:49:44 -0300

    exemplo novo

--- c_cpp/unidade_1/loops/exemplo_for.c
+++ c_cpp/unidade_1/loops/exemplo_for.c
@@ -2,12 +2,25 @@
 
 int main(){
     
-    int valor;
+    int valor, valor2;
     scanf("%d", &valor);
     for(int i = 1; i <= 10; i++){
-     printf("%d x %d = %d\n", valor, i, i*valor);
+        printf("%d x %d = %d\n", valor, i, i*valor);
+        printf("Digite 0 para continuar: ");
+        scanf("%d", &valor2);
+        if(valor2 != 0){
+            break;
+        }
     }
-   
+    printf("TCHAU\n"); 
+    printf("TCHAU\n"); 
+    printf("TCHAU\n"); 
+    printf("TCHAU\n"); 
+    printf("TCHAU\n"); 
+    printf("TCHAU\n"); 
+    printf("TCHAU\n"); 
+    printf("TCHAU\n"); 
+    
     
     return 0 ;
 }
--- c_cpp/unidade_1/loops/exemplo_while.c
+++ c_cpp/unidade_1/loops/exemplo_while.c
@@ -2,17 +2,20 @@
 
 int main(){
 
-    // int valor = -1;// variável de controle
-    // while (valor != 0){ // condição
-    //     printf("Digite 0 pra sair: ");
-    //     scanf("%d", &valor); // atualização
-    // }
+    int valor = -1;// variável de controle
+    while (1){ // condição 0 -> FALSO, 1 -> VERDADEIRO
+        printf("Digite -1 pra sair: ");
+        scanf("%d", &valor); // atualização
+        if(valor == -1){
+            break;
+        }
+    }
 
-    // int valor2 = 0;
-    // do{
-    //     printf("Digite 0 pra sair: ");
-    //     scanf("%d", &valor2);
-    // }while(valor2 != 0);
+    int valor2 = 0;
+    do{
+        printf("Digite 0 pra sair: ");
+        scanf("%d", &valor2);
+    }while(valor2 != 0);
     
 
     return 0;
