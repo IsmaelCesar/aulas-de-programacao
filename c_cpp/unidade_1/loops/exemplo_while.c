c0ed8f4e35cb47235b028daaf09249e2c5f34090
IsmaelCesar <leamscesar@gmail.com>
2025-05-22 13:09:38 -0300

    exemplos atualizados

---
+++ c_cpp/unidade_1/loops/exemplo_while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    // int valor = -1;// variável de controle
+    // while (valor != 0){ // condição
+    //     printf("Digite 0 pra sair: ");
+    //     scanf("%d", &valor); // atualização
+    // }
+
+    // int valor2 = 0;
+    // do{
+    //     printf("Digite 0 pra sair: ");
+    //     scanf("%d", &valor2);
+    // }while(valor2 != 0);
+    
+
+    return 0;
+}
