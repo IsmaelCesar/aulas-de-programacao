528ab5758e444ba8398b608d810e4c2b7967fce7
IsmaelCesar <leamscesar@gmail.com>
2025-05-27 14:37:45 -0300

    funcoes

--- c_cpp/unidade_1/funcoes/funcoes_e_prototipos.c
+++ c_cpp/unidade_1/funcoes/funcoes_e_prototipos.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
+int soma_um(int v1);
+float d_euclid(float xa, float ya, float xb, float yb);
+void imprime_mensagem(float x);
+float quadrado(float v1);
+
+int main(){
+    imprime_quadrados(11, 50);
+
+    // float x = 10, resultado;
+    // imprime_mensagem(50);
+    // printf("Valor de x: %.2f\n", x);
+    // printf("Valor antigo de x: %.2f\n", x);
+    // resultado = quadrado(x);
+    // printf("Valor novo de x: %.2f\n", x);
+    // printf("Valor de resultado: %.2f", resultado);
+    return 0;
+}
+
 int soma_um(int v1){
     v1++; // v1 = v1 + 1;
     return v1;
@@ -24,25 +42,4 @@ void imprime_quadrados(int m, int n){
     for(int i = m; i <= n; i++){
         printf(" %.2f\n ", quadrado((float) i));
     }
-}
-
-int ler_valores(){
-    int v;
-    printf("Digite algum valor: ");
-    scanf("%d", &v);
-    return v;
-}
-
-int main(){
-    imprime_quadrados(11, 50);
-    int resultado = meu_scan(0, 1000);
-
-    float x = 10, resultado;
-    imprime_mensagem(50);
-    printf("Valor de x: %.2f\n", x);
-    printf("Valor antigo de x: %.2f\n", x);
-    resultado = quadrado(x);
-    printf("Valor novo de x: %.2f\n", x);
-    printf("Valor de resultado: %.2f", resultado);
-    return 0;
 }
