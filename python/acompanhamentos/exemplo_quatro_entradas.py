2a63b3a2f3818da67c700e1212db4fbe33e3a00e
IsmaelCesar <leamscesar@gmail.com>
2025-06-18 14:17:57 -0300

    quatro entradas

--- python/acompanhamentos/exemplo_quatro_entradas.py
+++ python/acompanhamentos/exemplo_quatro_entradas.py
@@ -1,5 +1,5 @@
 # assumindo que o usuário digitará 4 inteiros
-# f1, f2, f3, f4 = map(int, input("Digite quatro inteiros:").split())
+f1, f2, f3, f4 = map(float, input().split())
 
 # print("Tipo f1: ", type(f1))
 # print("Tipo f2: ", type(f2))
